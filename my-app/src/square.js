00a1d4cc723a37111c8f08f572d0a376268f69e2
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-01-14 21:55:35 +0900

    React チュートリアル 概要 インタラクティブなコンポーネントを作る まで

---
+++ my-app/src/square.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          alert("click");
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+export default Square;
