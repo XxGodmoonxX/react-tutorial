7183b536a802a815be9c34c7329e0f856c53130a
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-01-16 00:34:35 +0900

    Stateのリフトアップ 途中まで

--- my-app/src/square.js
+++ my-app/src/square.js
@@ -1,20 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
-
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          alert("click");
+          this.props.onClick();
         }}
       >
         {this.props.value}
