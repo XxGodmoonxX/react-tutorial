cc3625228c706311574a38048555737b8f310557
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-01-15 23:57:32 +0900

    Squareにコンストラクタを追加しstateを初期化

--- my-app/src/square.js
+++ my-app/src/square.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
+
   render() {
     return (
       <button
