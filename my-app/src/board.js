6eda412dc39ef777f9829fbaa8331866da40447d
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-01-19 20:29:06 +0900

    3目並べ ゲームの勝者判定 実装

--- my-app/src/board.js
+++ my-app/src/board.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Square from "./square";
+import calculateWinner from "./calculateWinner";
 
 class Board extends React.Component {
   constructor(props) {
@@ -12,6 +13,11 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -31,7 +37,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner:" + winner;
+    } else {
+      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
