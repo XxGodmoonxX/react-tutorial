ec41b21f7243449b02a95b890d2dfb52da7f53a3
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2020-01-19 23:17:42 +0900

    Stateのリフトアップ BoardコンポーネントにあったStateをGameコンポーネントに移す

--- my-app/src/board.jsx
+++ my-app/src/board.jsx
@@ -3,65 +3,66 @@ import Square from "./square";
 import calculateWinner from "./calculateWinner";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // 配列9個用意して全部nullを入れる
-      squares: Array(9).fill(null),
-      // xIsNextというStateを用意 最初はtrue
-      xIsNext: true
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     // 配列9個用意して全部nullを入れる
+  //     squares: Array(9).fill(null),
+  //     // xIsNextというStateを用意 最初はtrue
+  //     xIsNext: true
+  //   };
+  // }
 
-  handleClick(i) {
-    // 配列を複製
-    const squares = this.state.squares.slice();
+  // handleClick(i) {
+  //   // 配列を複製
+  //   const squares = this.state.squares.slice();
 
-    // calculateWinnerの結果がtrue、もしくはクリックした盤面が既に中身が入っていたら
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+  //   // calculateWinnerの結果がtrue、もしくはクリックした盤面が既に中身が入っていたら
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     return;
+  //   }
 
-    // クリックされた盤面ににxIsTextの真偽によってXかOかを入れる
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      // stateであるsquaresに反映させる
-      squares: squares,
-      // stateのxIsNextの真偽を逆転させる
-      xIsNext: !this.state.xIsNext
-    });
-  }
+  //   // クリックされた盤面ににxIsTextの真偽によってXかOかを入れる
+  //   squares[i] = this.state.xIsNext ? "X" : "O";
+  //   this.setState({
+  //     // stateであるsquaresに反映させる
+  //     squares: squares,
+  //     // stateのxIsNextの真偽を逆転させる
+  //     xIsNext: !this.state.xIsNext
+  //   });
+  // }
 
   renderSquare(i) {
     return (
       <Square
         // propsを渡す
         // valueはstate XかOか
-        value={this.state.squares[i]}
+        // だったけど 今は親のprops
+        value={this.props.squares[i]}
         // クリックしたらhandleClickを実行する
+        // だったけど今は親のprops.onClick
         onClick={() => {
-          this.handleClick(i);
+          this.props.onClick(i);
         }}
       />
     );
   }
 
   render() {
-    // 変数winnerにcalculateWinnerの結果を入れる
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      // winnerがtrue、つまりなにか入っていれば
-      status = "Winner:" + winner;
-    } else {
-      // winnerがfalse、nullであれば
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
-    }
+    // // 変数winnerにcalculateWinnerの結果を入れる
+    // const winner = calculateWinner(this.state.squares);
+    // let status;
+    // if (winner) {
+    //   // winnerがtrue、つまりなにか入っていれば
+    //   status = "Winner:" + winner;
+    // } else {
+    //   // winnerがfalse、nullであれば
+    //   status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+    // }
 
     // レンダリングする内容
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -2,16 +2,74 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Board from "./board";
 import "./index.css";
+import calculateWinner from "./calculateWinner";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          // 配列9個用意して全部nullを入れる
+          squares: Array(9).fill(null)
+        }
+      ],
+      // xIsNextというStateを用意 最初はtrue
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    // stateのhistoryを代入
+    const history = this.state.history;
+    // historyの今のものを代入
+    const current = history[history.length - 1];
+    // 配列を複製
+    const squares = current.squares.slice();
+    // calculateWinnerの結果がtrue、もしくはクリックした盤面が既に中身が入っていたら
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    // クリックされた盤面ににxIsTextの真偽によってXかOかを入れる
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    // stateをSetする
+    this.setState({
+      // historyに新しくsquaresの配列をつなげる
+      history: history.concat([
+        {
+          // stateであるsquaresに反映させる
+          squares: squares
+        }
+      ]),
+      // stateのxIsNextの真偽を逆転させる
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    // stateのhistoryを代入
+    const history = this.state.history;
+    // historyの今のものを代入
+    const current = history[history.length - 1];
+    // 変数winnerにcalculateWinnerの結果を入れる
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      // winnerがtrue、つまりなにか入っていれば
+      status = "Winner:" + winner;
+    } else {
+      // winnerがfalse、nullであれば
+      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
